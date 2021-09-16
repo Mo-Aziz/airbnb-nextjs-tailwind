0b9426d0f77ffb9f05aa0d84ef0c0ede7befa84c
MJ <mohamed.aref32@gmail.com>
2021-09-15 23:32:01 -0500

    finished the header with the search input and user icons

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className=" ">
+    <div className="">
       <Head>
         <title>Airbnb-clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 {/* header */}
-{/* banner */}
-{/* explore near by */}
-
-       
+      <Header />
+      {/* banner */}
+<Banner/>
+      {/* explore near by */}
     </div>
-  )
+  );
 }
