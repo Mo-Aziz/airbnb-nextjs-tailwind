19f1d6c3b1d5ab74a8b5e2fcda2cbdf005dba485
MJ <mohamed.aref32@gmail.com>
2021-09-16 20:27:52 -0500

    added live anywhere section 3

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -20,9 +21,11 @@ export default function Home({ exploreData }) {
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
           {/* pulling the cards data from the server */}
-          <div className="grid
+          <div
+            className="grid
            grid-cols-1
-            sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+          >
             {exploreData?.map(({ img, distance, location }) => (
               <SmallCard
                 key={img}
@@ -30,6 +33,18 @@ export default function Home({ exploreData }) {
                 distance={distance}
                 location={location}
               />
+            ))}
+          </div>
+        </section>
+
+        {/* section 3 live anywhere -meduim card- */}
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div className="flex space-x-3 overflow-scroll
+          scrollbar-hide p-3 -ml-3">
+            {cardsData?.map((item) => (
+              <MediumCard id={item.img} img={item.img} title={item.title} />
             ))}
           </div>
         </section>
@@ -42,9 +57,13 @@ export async function getStaticProps() {
   const exploreData = await fetch("https://links.papareact.com/pyp").then(
     (res) => res.json()
   );
+  const cardsData = await fetch("https://links.papareact.com/zp1").then(
+    (res) => res.json()
+  );
   return {
     props: {
       exploreData,
+      cardsData
     },
   };
 }
